5300d8037b3dbab1e0a81339ae89bf3102e3a7c0
kidist1234 <kidistmamaye2002@gmail.com>
2022-10-14 07:18:15 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch = 'a', ch1 = 'A';
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+for (ch1 = 'A'; ch1 <= 'Z'; ch1++)
+{
+putchar(ch1);
+}
+return (0);
+}
