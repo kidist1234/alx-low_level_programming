098a5f2df931da00e2fb8d424f0d684e5887215a
kidist1234 <kidistmamaye2002@gmail.com>
2022-10-14 10:13:09 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry poit
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num;
+char ch;
+for (num = 0; num <= 9; num++)
+{
+putchar((num % 10) + '0');
+}
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
