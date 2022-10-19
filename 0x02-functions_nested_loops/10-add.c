e7d0e3906eb6b30fabaa3698644c60552d437c03
kidist1234 <kidistmamaye2002@gmail.com>
2022-10-19 09:29:06 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * add - Entry point
+ *
+ * @a: character
+ *
+ * @b: character
+ *
+ * Description: 'sum'
+ *
+ * Return: Always 0 (Success)
+ */
+int add(int a, int b)
+{
+int sum;
+sum = a + b;
+return (sum);
+}
