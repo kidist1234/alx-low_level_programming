4c1208835bb083a878ce0cb8f0b7310f9f71dc95
kidist1234 <kidistmamaye2002@gmail.com>
2022-10-20 03:10:00 -0700

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - Entry point
+ *
+ * @a: number
+ * @b: number
+ * Description: 'multiplication'
+ *
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
