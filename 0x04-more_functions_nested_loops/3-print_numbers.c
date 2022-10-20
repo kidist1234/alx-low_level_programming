98943a8f2dce7210e286c2395982e174b2e49b24
kidist1234 <kidistmamaye2002@gmail.com>
2022-10-20 05:37:39 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - Entry point
+ *
+ * Description: 'print 0 - 9'
+ *
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+{
+_putchar(n + '0');
+}
+_putchar('\n');
+}
