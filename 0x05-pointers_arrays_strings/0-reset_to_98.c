fbab7801a357980dd0dda8cb8dab9620676110ed
kidist1234 <kidistmamaye2002@gmail.com>
2022-10-24 00:57:50 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - Entry point
+ *
+ * @n: character
+ *
+ * Description: 'reset to 98'
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,3 @@
+#include <unistd.h>
+int _putchar(char c);
+void reset_to_98(int *n);
