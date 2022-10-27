32c698a9df71aa3d098e71b4b36ef788f8c2172f
kidist1234 <kidistmamaye2002@gmail.com>
2022-10-27 03:02:22 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,16 +12,17 @@
 char *_strncat(char *dest, char *src, int n)
 {
 int i = 0;
-int j;
+int j = 0;
 while (dest[i])
 {
 i++;
 }
-for (j = 0; j < n ; j++)
+while (j < n && src[j])
 {
 dest[i] = src[j];
 i++;
+j++;
 }
-dest[i] = '\0';
+dest[i + n + 1] = '\0';
 return (dest);
 }
