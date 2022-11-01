5f096e5d2c96075daf12e287c8bff633ed228dfd
kidist1234 <kidistmamaye2002@gmail.com>
2022-11-01 01:43:41 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - Entry point
+ *
+ * Description: 'fills memory'
+ * @s: character
+ * @b: constant byte
+ * @n: size
+ *
+ * Return: Always 0 (Success)
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int j;
+for (j = 0; j < n; j++)
+{
+s[i] = b;
+}
+return (s);
+}
