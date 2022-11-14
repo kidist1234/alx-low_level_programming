01273b9abec2d979e24a076a52f8840d8994da34
kidist1234 <kidistmamaye2002@gmail.com>
2022-11-14 04:18:34 -0800

    read me for struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_HEADER
+#define DOG_HEADER
+/**
+ * struct dog - struct for dog information
+ * @name: character
+ * @age: float
+ * @owner: chatacter
+ */
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+}dog_f;
+#endif
+
