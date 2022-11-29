a451c25b7ef907328bfdc1c167ccb72e84818f3c
kidist1234 <kidistmamaye2002@gmail.com>
2022-11-29 12:36:03 -0800

    lists header

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adding node
+ * @head: pointer pointing the listint_t
+ * @n: the values of the structure
+ * Return: return the new element address
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+listint_t *new = malloc(sizeof(listint_t));
+if(!new)
+return (NULL);
+new->next = *head;
+new->n = n;
+*head = new;
+return (new);
+}
